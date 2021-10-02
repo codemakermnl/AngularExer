f1b26027cc3ced3f954cff53e5a89f5613de6631
pagawaanNgCode <winstonmcgee696@gmail.com>
2021-10-02 13:46:14 +0800

    Added sizes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent{
   title = 'angular-exer';
 
   name:string = '';
@@ -13,7 +13,17 @@ export class AppComponent {
   address:string = '';
   course:string = '';
 
-  pizza = ['Cheese', 'Veggie', 'Pepperoni', 'Hawaiian', 'Meat', 'Margherita', 'BBQ Chicken', 'Supreme'];
-
+  pizza1 = ['Cheese', 'Veggie', 'Pepperoni', 'Hawaiian', 'Meat', 'Margherita', 'BBQ Chicken', 'Supreme'];
+  pizza : any = [ {"name": 'Cheese', "size":'Regular'} ,
+            {"name": 'Veggie', "size":'Medium'},
+            {"name": 'Pepperoni', "size":'Family'},
+            {"name": 'Hawaiian', "size":'Large'},
+            {"name": 'Meat', "size":'Large'},
+            {"name": 'Margherita', "size":'Regular'},
+            {"name": 'BBQ Chicken', "size":'Medium'},
+            {"name": 'Supreme', "size":'Regular'},
+            {"name": 'Cheese', "size":'Family'}, ];
   
+
 }
+
